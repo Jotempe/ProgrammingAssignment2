1546b8958051fa6e37673fec248a62a58c892143
jotempe <jmp.misio@gmail.com>
2014-07-24 18:01:15 +0200

    Beautify code, some typos in comments fixed

--- cachematrix.R
+++ cachematrix.R
@@ -8,17 +8,17 @@ makeCacheMatrix <- function(x = matrix()) {
 	## This is needed to create cache variable in the current scope
 	inv <- NULL
 	
-	## Method to change the original matrix
+	## Method to modify the original matrix
 	set <- function(y) {
 		x <<- y
 		## invalidate the cached inverse when matrix changed
 		inv <<- NULL
 		}
 	
-	## Metrod to retun the matrix
+	## Method returns the original matrix
 	get <- function() x
 	
-	## Now, that is what the assingment required, but making the setinv function public seems
+	## Now, that is what the assignment required, but making the setinv function public seems
 	## not the right way of doing it - anybody can then set the "cached inverse" to whatever
 	## they want. IMHO it would make more sense to have getinv do what cacheSolve does, and drop 
 	## setinv and cacheSolve completely...
@@ -27,22 +27,27 @@ makeCacheMatrix <- function(x = matrix()) {
 	## method to return the cached inverse, supposed to be called only by cacheSolve
 	getinv <- function() inv
 	
-	list(set = set, get = get,
-             setinv = setinv,
-             getinv = getinv)
+	## Return the list of methods
+	list(set = set, 
+		 get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 ## Return inverse of a matrix created by makeCacheMatrix, using cached result
-## (if it exists). Write result to chache. 
-## Now, there seems to be a bit of inconsistency here... The function uses ... syntax to pass extra
+## (if it exists). Write result to cache. 
+## Now, there seems to be a bit of inconsistency here. The function uses ... syntax to pass extra
 ## parameters to solve(). The result is, however, cached and returned later regardless of those parameters,
-## thus it is possible that the cached result is not what has been actually requested. Forget it for now...
+## thus it is possible that the cached result is not what has been actually requested (calling the function
+## second time with different parameters set as ... will return the same result). But apparently we are
+## supposed to disregard this in the solution.
 
 cacheSolve <- function(x, ...) {
 
+	## Do we have cached inverse?
 	inv <- x$getinv()
 	if (!is.null(inv)) {
-        ## Return cached inverse
+        ## Return cached data
 		return(inv)
 		}
 	## else solve the matrix, cache the result and return it
