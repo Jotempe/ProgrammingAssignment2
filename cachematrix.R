0576e0ff1eccc236ff3f620959dcbdd59f7296af
jotempe <jmp.misio@gmail.com>
2014-07-24 16:48:44 +0200

    As required in the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## How to make R "object oriented" (kind of)
+## create a matrix with ability to cache its inverse
 
-## Write a short comment describing this function
+## create an "object" with two properties (the matrix and its inverse) and four methods
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	## This is needed to create cache variable in the current scope
+	inv <- NULL
+	
+	## Method to change the original matrix
+	set <- function(y) {
+		x <<- y
+		## invalidate the cached inverse when matrix changed
+		inv <<- NULL
+		}
+	
+	## Metrod to retun the matrix
+	get <- function() x
+	
+	## Now, that is what the assingment required, but making the setinv function public seems
+	## not the right way of doing it - anybody can then set the "cached inverse" to whatever
+	## they want. IMHO it would make more sense to have getinv do what cacheSolve does, and drop 
+	## setinv and cacheSolve completely...
+	setinv <- function(y) inv <<- y
+	
+	## method to return the cached inverse, supposed to be called only by cacheSolve
+	getinv <- function() inv
+	
+	list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## Return inverse of a matrix created by makeCacheMatrix, using cached result
+## (if it exists). Write result to chache. 
+## Now, there seems to be a bit of inconsistency here... The function uses ... syntax to pass extra
+## parameters to solve(). The result is, however, cached and returned later regardless of those parameters,
+## thus it is possible that the cached result is not what has been actually requested. Forget it for now...
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	inv <- x$getinv()
+	if (!is.null(inv)) {
+        ## Return cached inverse
+		return(inv)
+		}
+	## else solve the matrix, cache the result and return it
+	inv <- solve(x$get(),...)
+	x$setinv(inv)
+	inv
 }
